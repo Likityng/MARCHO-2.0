e5b9254aa1d59678870a51a9452c59b11acc63cc
Likityng <roman.lebedev.94@list.ru>
2022-06-08 22:20:36 +0300

    added header,slider,promo,footer,product

---
+++ task/html.js
@@ -0,0 +1,36 @@
+const { src, dest } = require("gulp");
+
+// Конфтгурации
+const path = require("../config/path.js");
+const app = require("../config/app.js");
+
+// Плагины
+const plumber = require("gulp-plumber");
+const notify = require("gulp-notify");
+const fileInclude = require("gulp-file-include");
+const htmlmin = require("gulp-htmlmin");
+const size = require("gulp-size");
+const webpHtml = require("gulp-webp-html");
+
+// Обработка HTML
+const html = () => {
+  return src(path.html.src)
+    .pipe(plumber({
+      errorHandler: notify.onError(error => ({
+        title: "HTML",
+        message: error.message
+      }))
+    }))
+    .pipe(fileInclude())
+    .pipe(webpHtml())
+    .pipe(size({
+      title: "До"
+    }))
+    .pipe(htmlmin(app.htmlmin))
+    .pipe(size({
+      title: "После"
+    }))
+    .pipe(dest(path.html.dest));
+}
+
+module.exports = html;
